43bc236ab3b7ac977dfddc216cdf1f1d31a42d12
c2y2 <lexiangtaotao1988@gmail.com>
2014-07-15 22:13:46 +0800

    update add_route.sh

--- route_add.sh
+++ route_add.sh
@@ -7,4 +7,7 @@ route add -net 115.239.210.0/24 gw 192.168.28.1 dev eth1;
 route add -net 119.75.219.0/24 gw 192.168.28.1 dev eth1;
 route add -net 180.76.3.0/24 gw 192.168.28.1 dev eth1;
 route add -net 192.168.30.0/24 gw 192.168.28.1 dev eth1;
-
+route add -net 180.76.2.0/24 gw 192.168.28.1 dev eth1;
+route add -net 122.143.13.0/24 gw 192.168.28.1 dev eth1;
+route add -net 173.194.127.0/24 gw 192.168.28.1 dev eth1;
+route add -net 61.135.185.112.0/24 gw 192.168.28.1 dev eth1;
