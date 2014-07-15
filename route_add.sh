8606433af6b40ddd448e4dc9ebafe194a0e5dbde
c2y2 <lexiangtaotao1988@gmail.com>
2014-07-15 22:23:25 +0800

    del 173.194.127.*

--- route_add.sh
+++ route_add.sh
@@ -9,6 +9,5 @@ route add -net 180.76.3.0/24 gw 192.168.28.1 dev eth1;
 route add -net 192.168.30.0/24 gw 192.168.28.1 dev eth1;
 route add -net 180.76.2.0/24 gw 192.168.28.1 dev eth1;
 route add -net 122.143.13.0/24 gw 192.168.28.1 dev eth1;
-route add -net 173.194.127.0/24 gw 192.168.28.1 dev eth1;
 route add -net 61.135.185.112.0/24 gw 192.168.28.1 dev eth1;
 route add -net 115.239.211.0/24 gw 192.168.28.1 dev eth1;
