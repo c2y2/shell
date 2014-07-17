54ceb804024a80063edddafb35c7e290428e622b
c2y2 <lexiangtaotao1988@gmail.com>
2014-07-17 20:56:16 +0800

    add linux_for_route.sh

---
+++ linux_for_route.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ifconfig eth0 192.168.29.1 netmask 255.255.255.0;
+route add -net 192.168.29.0/24 dev eth0;
+echo 1 > /proc/sys/net/ipv4/ip_forward;
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE;
+ifconfig eth0 up
+exit 0;
+
